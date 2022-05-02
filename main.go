aff75fa2671f370a7971343ecbff4fa1c11d7277
L2ncE <llance_24@foxmail.com>
2022-05-02 16:55:21 +0800

    feat(main):初始化主函数

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"wechat/api"
+	"wechat/dao"
+)
+
+func main() {
+	err := dao.InitGormDB()
+	if err != nil {
+		fmt.Printf("init redis failed, err:%v\n", err)
+	} else {
+		fmt.Println("连接GORM MySQL数据库成功!")
+	}
+	api.InitEngine()
+}
