8a1f5c9a2efc4303e72ffcb85fc6f8087fbe49b8
L2ncE <llance_24@foxmail.com>
2022-05-03 16:13:38 +0800

    refactor(router):重构路由，新增获取openid接口

--- api/router.go
+++ api/router.go
@@ -9,6 +9,8 @@ func InitEngine() {
 	engine := gin.Default()
 	engine.Use(CORS())
 
+	engine.POST("/openid", getOpenId)
+
 	err := engine.Run(":5556")
 	if err != nil {
 		log.Printf("init error:%v\n", err)
