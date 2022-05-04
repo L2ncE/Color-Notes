7eb6e983de9818b649b699c4cb62f9b82468ce32
L2ncE <llance_24@foxmail.com>
2022-05-04 15:44:04 +0800

    feat(jwt):增加jwt验证功能

--- api/middleware.go
+++ api/middleware.go
@@ -18,11 +18,11 @@ func JWTAuth(ctx *gin.Context) {
 		ctx.Abort()
 		return
 	}
-	ctx.Set("id", ParseToken(token))
+	ctx.Set("openid", ParseToken(token))
 	ctx.Next()
 }
 
-func ParseToken(s string) int {
+func ParseToken(s string) string {
 	//解析传过来的token
 	tokenClaims, err := jwt.ParseWithClaims(s, &model.MyClaims{}, func(token *jwt.Token) (interface{}, error) {
 		return mySigningKey, nil
@@ -30,7 +30,7 @@ func ParseToken(s string) int {
 	if err != nil {
 		fmt.Println(err)
 	}
-	return tokenClaims.Claims.(*model.MyClaims).ID
+	return tokenClaims.Claims.(*model.MyClaims).OpenId
 }
 
 //CORS 跨域中间件
--- model/jwt.go
+++ model/jwt.go
@@ -3,8 +3,7 @@ package model
 import "github.com/dgrijalva/jwt-go"
 
 type MyClaims struct {
-	ID       int    `json:"ID"`
-	Username string `json:"username"`
-	Password string `json:"password"`
+	ID     int    `json:"ID"`
+	OpenId string `json:"OpenId"`
 	jwt.StandardClaims
 }
