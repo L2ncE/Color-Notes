d241ce1ddb7cdc2330346d6a9e587bdcc3dd8bf3
L2ncE <llance_24@foxmail.com>
2022-05-25 17:39:28 +0800

    feat(dao):增加redis注册

--- dao/redis/user.go
+++ dao/redis/user.go
@@ -2,11 +2,20 @@ package redis
 
 import "log"
 
-func IsRegister(userID int) bool { //当前用户是否注册
-	flag, err := rdb.SIsMember("openid", userID).Result()
+func IsRegister(id string) (bool, error) { //当前用户是否注册
+	flag, err := rdb.SIsMember("openid", id).Result()
 	if err != nil {
-		log.Println("isRegister err:", err)
-		return true
+		log.Println("redis judge is register err:", err)
+		return false, err
 	}
-	return flag
+	return flag, nil
+}
+
+func AddOpenId(id string) error {
+	err := rdb.SAdd("openid", id).Err()
+	if err != nil {
+		log.Println("redis add openid err", err)
+		return err
+	}
+	return nil
 }
