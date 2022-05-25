e067c8ce96836d86538101ceb247af27e0098a6c
L2ncE <llance_24@foxmail.com>
2022-05-25 15:48:11 +0800

    refactor:重构相关路劲

--- service/user.go
+++ service/user.go
@@ -2,16 +2,16 @@ package service
 
 import (
 	"gorm.io/gorm"
-	"wechat/dao"
+	"wechat/dao/mysql"
 )
 
 func RegisterUser(openId string) error {
-	err := dao.InsertOpenId(openId)
+	err := mysql.InsertOpenId(openId)
 	return err
 }
 
 func IsRepeatOpenId(openId string) (bool, error) {
-	err := dao.SelectUserByOpenId(openId)
+	err := mysql.SelectUserByOpenId(openId)
 	if err != nil {
 		if err == gorm.ErrRecordNotFound {
 			return false, nil
