65dbe19d096722ec39b22d2b935e99701b556d2d
L2ncE <llance_24@foxmail.com>
2022-05-26 16:57:26 +0800

    feat(user):用户相关新增redis缓存，新方案

--- dao/redis/user.go
+++ dao/redis/user.go
@@ -1,6 +1,9 @@
 package redis
 
-import "log"
+import (
+	"log"
+	"wechat/dao/mysql"
+)
 
 func IsRegister(id string) (bool, error) { //当前用户是否注册
 	flag, err := rdb.SIsMember("openid", id).Result()
@@ -16,6 +19,34 @@ func AddOpenId(id string) error {
 	if err != nil {
 		log.Println("redis add openid err", err)
 		return err
+	}
+	return nil
+}
+
+func IsOpenIdCache() (bool, error) {
+	es, err := rdb.SMembers("openid").Result()
+	if err != nil {
+		log.Println("openid cache get error:", err)
+		return false, err
+	}
+	if len(es) > 0 {
+		return true, nil
+	}
+	return false, nil
+}
+
+func MoveOpenIdToMySQL() error {
+	set, err := rdb.SMembers("openid").Result()
+	if err != nil {
+		log.Println("len of openid get error:", err)
+		return err
+	}
+	for _, v := range set {
+		err := mysql.InsertOpenId(v)
+		if err != nil {
+			log.Println("insert err:", err)
+			break
+		}
 	}
 	return nil
 }
--- service/user.go
+++ service/user.go
@@ -3,17 +3,36 @@ package service
 import (
 	"gorm.io/gorm"
 	"wechat/dao/mysql"
+	"wechat/dao/redis"
 )
 
 func RegisterUser(openId string) error {
-	err := mysql.InsertOpenId(openId)
+	flag, err := redis.IsOpenIdCache()
+	err = redis.AddOpenId(openId)
+	if flag {
+		err = redis.AddOpenId(openId)
+		return err
+	}
+	err = mysql.InsertOpenId(openId)
 	return err
 }
 
 func IsRepeatOpenId(openId string) (bool, error) {
-	err := mysql.SelectUserByOpenId(openId)
+	flag, err := redis.IsOpenIdCache()
 	if err != nil {
-		if err == gorm.ErrRecordNotFound {
+		return true, err
+	}
+	if flag { //有缓存
+		flag, err = redis.IsRegister(openId) //查看是否注册过
+		if err != nil {
+			return true, err
+		}
+		return flag, nil
+	}
+	err = mysql.SelectUserByOpenId(openId)
+
+	if err != nil {
+		if err == gorm.ErrRecordNotFound { //找不到会报这个错误捏
 			return false, nil
 		}
 		return false, err
