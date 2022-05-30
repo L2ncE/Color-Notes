046e5814f37dc6c475ce44056e814f0a564fbcc3
L2ncE <llance_24@foxmail.com>
2022-05-30 10:31:46 +0800

    refactor(config):重构配置

--- model/config.go
+++ model/config.go
@@ -1,12 +1,13 @@
 package model
 
 type ServerConfig struct {
-	Name      string      `mapstructure:"name"`
-	Port      int         `mapstructure:"port"`
-	APPID     string      `mapstructure:"appID"`
-	APPSecret string      `mapstructure:"appSecret"`
-	GormInfo  GormConfig  `mapstructure:"gorm"`
-	RedisInfo RedisConfig `mapstructure:"redis"`
+	Name        string        `mapstructure:"name"`
+	Port        int           `mapstructure:"port"`
+	APPID       string        `mapstructure:"appID"`
+	APPSecret   string        `mapstructure:"appSecret"`
+	GormInfo    GormConfig    `mapstructure:"gorm"`
+	RedisInfo   RedisConfig   `mapstructure:"redis"`
+	MongoDBInfo MongoDBConfig `mapstructure:"mongodb"`
 }
 
 type GormConfig struct {
@@ -22,4 +23,10 @@ type RedisConfig struct {
 	Port     int    `mapstructure:"port"`
 	Password string `mapstructure:"password"`
 	DB       int    `mapstructure:"db"`
+}
+
+type MongoDBConfig struct {
+	Host   string `mapstructure:"host"`
+	Port   int    `mapstructure:"port"`
+	DBName string `mapstructure:"dbName"`
 }
