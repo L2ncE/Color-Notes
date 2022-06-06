0c21b97c553f2dabb93c9a6babd60955c0e54fea
L2ncE <llance_24@foxmail.com>
2022-06-06 15:15:20 +0800

    refactor(router):重构路由，新增笔记本相关

--- api/router.go
+++ api/router.go
@@ -11,7 +11,16 @@ func InitEngine() {
 	engine := gin.Default()
 	engine.Use(CORS())
 
-	engine.POST("/user/signup", test)
+	engine.POST("/user/signup", getOpenId)
+	engine.POST("/test", test)
+
+	notebookGroup := engine.Group("/notebook")
+	{
+		notebookGroup.Use(JWTAuth)
+		notebookGroup.POST("/create", createNotebook)
+		notebookGroup.PUT("/update/name/:id", changeNotebookName)
+		notebookGroup.PUT("/update/color/:id", changeNotebookColor)
+	}
 
 	err := engine.Run(fmt.Sprintf(":%d", global.Settings.Port))
 	if err != nil {
