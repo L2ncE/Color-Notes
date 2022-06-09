659c81ee6b59bc8e4064b84895ec056ab60b3d16
L2ncE <llance_24@foxmail.com>
2022-06-09 15:14:56 +0800

    refactor(api):路由重构

--- api/router.go
+++ api/router.go
@@ -20,6 +20,26 @@ func InitEngine() {
 		notebookGroup.POST("/create", createNotebook)
 		notebookGroup.PUT("/update/name/:id", changeNotebookName)
 		notebookGroup.PUT("/update/color/:id", changeNotebookColor)
+		notebookGroup.DELETE("/delete/:id", deleteNotebook)
+	}
+
+	noteGroup := engine.Group("/note")
+	{
+		noteGroup.GET("/like/:id", getNoteLikeCount)
+		noteGroup.GET("/agree/:id", getNoteAgreeCount)
+		noteGroup.GET("/:id", getNote)
+		noteGroup.GET("/notebook/:id", getNoteByNotebook)
+		{
+			noteGroup.Use(JWTAuth)
+			noteGroup.POST("/upload", uploadNote)
+			noteGroup.POST("/release/:id", releaseNote)
+			noteGroup.POST("/like/:id", LikeUpNote)
+			noteGroup.POST("/agree/:id", AgreeNote)
+			noteGroup.PUT("/update/delta/:id", updateNoteDelta)
+			noteGroup.PUT("/update/notebook/:id", changeNoteBook)
+			noteGroup.PUT("/update/name/:id", changeNoteName)
+			noteGroup.DELETE("/delete/:id", deleteNote)
+		}
 	}
 
 	err := engine.Run(fmt.Sprintf(":%d", global.Settings.Port))
