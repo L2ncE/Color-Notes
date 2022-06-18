1e35706ce5b40f7e1b6d781a16cd0e7b0285d303
L2ncE <llance_24@foxmail.com>
2022-06-18 11:59:20 +0800

    feat(model):新增社区模型

--- model/note.go
+++ model/note.go
@@ -3,12 +3,12 @@ package model
 import "time"
 
 type Note struct {
-	NoteId     int       `gorm:"column:noteId"`
-	OpenId     string    `gorm:"column:openId"`
-	NoteName   string    `gorm:"column:noteName"`
-	LastUpdate time.Time `gorm:"column:lastUpdate"`
-	Release    int       `gorm:"column:release"`
-	NoteBookId int       `gorm:"column:noteBookId"`
+	NoteId        int       `gorm:"column:noteId"`
+	OpenId        string    `gorm:"column:openId"`
+	NoteName      string    `gorm:"column:noteName"`
+	LastUpdate    time.Time `gorm:"column:lastUpdate"`
+	ReleaseStatus int       `gorm:"column:releaseStatus"`
+	NoteBookId    int       `gorm:"column:noteBookId"`
 }
 
 type NoteContent struct {
